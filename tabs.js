2be7de4d7e2a26d1112bcec75a9e37e9eb340094
ViktorOs <viktorosa@gmail.com>
2016-04-28 12:50:53 +0300

    first

---
+++ tabs.js
@@ -0,0 +1,32 @@
+/*
+    <div>
+        <div class="news-main-tab">
+            <a href="javascript:;" class="tab active" data-tab="1">
+                1
+            </a>
+                    <a href="javascript:;" class="tab" data-tab="2">
+                2
+            </a>
+                    <a href="javascript:;" class="tab" data-tab="3">
+                3
+            </a>
+        </div>
+
+        <div class="news-main-list" data-list="1">  
+            1      
+        </div>
+        <div class="news-main-list" data-list="2"> 
+            2       
+        </div>
+        <div class="news-main-list" data-list="3"> 
+            3       
+        </div>
+    </div>
+*/
+$(" .tab").click(function(){
+    $(this).parent().find(".tab").removeClass("act");
+    $(this).addClass("act");
+    $(this).parent().parent().find("div[data-list]").hide();        
+    var _tab = $(this).data("tab")   
+    $(this).parent().parent().find("div[data-list='"+_tab+"']").show();         
+})
